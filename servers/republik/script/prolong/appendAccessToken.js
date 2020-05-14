dd14a5364a2e77aab5bde675500ac6288c4d3f7e
Patrick Venetz <venetz@gmail.com>
2020-05-14 12:52:33 +0200

    wip: Append script

--- servers/republik/script/prolong/appendAccessToken.js
+++ servers/republik/script/prolong/appendAccessToken.js
@@ -3,6 +3,7 @@ require('@orbiting/backend-modules-env').config()
 
 const { csvFormat, csvParse } = require('d3-dsv')
 const { transformUser, AccessToken } = require('@orbiting/backend-modules-auth')
+const { encode } = require('@orbiting/backend-modules-base64u')
 const fs = require('fs').promises
 const path = require('path')
 const PgDb = require('@orbiting/backend-modules-base/lib/PgDb')
@@ -33,23 +34,42 @@ PgDb.connect().then(async pgdb => {
 
   const listArray = csvParse(listRaw)
 
-  const users = await pgdb.public.users.find({ id: listArray.map(row => row[argv.identifier]) })
+  /* const users = await pgdb.public.users.find({ id: listArray.map(row => row[argv.identifier]) })
+    .then(users => users.map(transformUser)) */
+
+  const users = await pgdb.public.users.find({ email: listArray.map(row => row[argv.identifier]) })
     .then(users => users.map(transformUser))
 
+  const accessGrants = await pgdb.public.accessGrants.find({
+    recipientUserId: users.map(u => u.id),
+    accessCampaignId: '3d0fc18e-0c80-46a9-ac8a-a697f912877f'
+  })
+
+  console.warn('users: ', users.length, 'accessGrants:', accessGrants.length)
+
   const list = await Promise.map(listArray, async (row, index) => {
-    const user = users.find(user => user.id === row[argv.identifier])
+    if (index % 100 === 1) {
+      console.warn(`  ${index + 1}`)
+    }
+
+    const user = users.find(user => user.email.toLowerCase() === row[argv.identifier].toLowerCase())
+    const accessGrant = user && accessGrants.find(a => a.recipientUserId === user.id)
 
     if (!user) {
-      throw new Error(`User with ID "${row[argv.identifier]}" not found.`)
+      console.warn(`User with ID "${row[argv.identifier]}" not found.`)
+      return
+      // throw new Error(`User with ID "${row[argv.identifier]}" not found.`)
     }
 
     const token = await AccessToken.generateForUser(user, argv.scope)
 
     return {
-      ...row,
-      CP_ATOKEN: token
+      email: user.email,
+      EMAILB64U: encode(user.email),
+      AS_ATOKEN: token,
+      COVID19_AG: accessGrant ? 'Y' : ''
     }
-  }, { concurrency: 1 })
+  })
 
   console.log(csvFormat(list.filter(Boolean)))
 
